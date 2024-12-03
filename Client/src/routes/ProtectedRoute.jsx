83903c843e993f3cd0918bb4eef8afbf22c6bbce
DiegoAlvaradoUCA <00127522@uca.edu.sv>
2024-12-02 23:17:04 -0600

    Arreglado, falta admin y notas

--- Client/src/routes/ProtectedRoute.jsx
+++ Client/src/routes/ProtectedRoute.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../provider/AuthContext';
-import { useRole } from '../provider/RoleProvider';
+import { RoleProvider, useRole } from '../provider/RoleProvider';
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
-  const { user } = useAuth();  // Usamos el contexto de autenticación para verificar si el usuario está autenticado
-  const { role } = useRole();   // Usamos el contexto de roles para verificar el rol del usuario
+  const { role } = useRole();
 
-  // Si el usuario no ha sido verificado aún
-  if (user === undefined) {
-    return <div>Verificando autenticación...</div>;
+   if (user === undefined) {
+     return <div>Verificando autenticación...</div>;
+   }
+
+   if (!user) {
+     return <Navigate to="/login" replace />;
   }
 
-  // Si no hay usuario autenticado, redirige al login
-  if (!user) {
-    return <Navigate to="/login" replace />;
-  }
-
-  // Si el rol no es permitido, redirige a la página principal
   if (allowedRoles && !allowedRoles.includes(role)) {
     return <Navigate to="/" replace />;
   }
 
-  // Si el usuario está autenticado y tiene el rol adecuado, renderiza el contenido
   return children;
 };
 
